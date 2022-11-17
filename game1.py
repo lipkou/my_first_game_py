3a2c7d9f8302caf5caa80631c6484585a45bfaa8
Lipkou <d.lipkous@gmail.com>
2022-11-17 17:18:56 +0200

    понадобавляв картинок (наступний рівень) + музкика

--- game1.py
+++ game1.py
@@ -1,11 +1,12 @@
 import pygame
-from time import sleep
+from time import sleep, time
 pygame.init()
 
 #! sdfsdfdsfsd
 #? sdfsdfsdf
 #* dfsdf
 #// хуй
+
 GREEN = (0, 200, 0)
 BLUE = (0, 0, 200)
 LIGHT_BLUE = (50, 50, 220)
@@ -27,17 +28,21 @@ clock = pygame.time.Clock()
 img_level_1 = pygame.image.load('images//level_1.jpg')
 img_level_1 = pygame.transform.scale(img_level_1, (WIN_WIDTH, WIN_HEIGHT))
 
-win_image = pygame.image.load('images//win_image.png')
-win_image = pygame.transform.scale(win_image, (WIN_WIDTH, WIN_HEIGHT))
 
 img_level_2 = pygame.image.load('images//level_2.jpg')
 img_level_2 = pygame.transform.scale(img_level_2, (WIN_WIDTH, WIN_HEIGHT))
+
+next_level_image = pygame.image.load('images//next_level_v2.jpg')
+next_level_image = pygame.transform.scale(next_level_image, (WIN_WIDTH, WIN_HEIGHT))
+
+win_image = pygame.image.load('images//win_image.png')
+win_image = pygame.transform.scale(win_image, (WIN_WIDTH, WIN_HEIGHT))
 
 pygame.mixer.music.load('music//fon_music.ogg')
 pygame.mixer.music.set_volume(0.05)
 pygame.mixer.music.play(-1)
 
-music_win = pygame.mixer.Sound('music//fon_music.ogg')
+music_win = pygame.mixer.Sound('music//you_win.wav')
 music_gg = pygame.mixer.Sound('music//GG.ogg')
 music_use = pygame.mixer.Sound('music//Button_use.ogg')
 music_pick_up_coin = pygame.mixer.Sound('music//pick_up_coin.ogg')
@@ -54,7 +59,7 @@ class Button():
 
     def show(self):
         pygame.draw.rect(window, self.color, self.rect)
-        window.blit(self.txt, (self.rect.x + 50, self.rect.y + 30))
+        window.blit(self.txt, (self.rect.x + 50, self.rect.y + 20))
 
 class Area():
     def __init__(self, x, y, width, height, color): #конструктор прямокутника
@@ -63,7 +68,6 @@ class Area():
     def draw_rect(self): #робить прямокутник
         pygame.draw.rect(window, self.color, self.rect)
 
-
 class Label(Area):
     def set_text(self, text): #настройка тексту
         self.font_40 = pygame.font.SysFont('Arial', 30)
@@ -71,7 +75,6 @@ class Label(Area):
     def draw_label(self, shift_x, shift_y): #виведення тексту на екран
         self.draw_rect()
         window.blit(self.text, (self.rect.x + shift_x, self.rect.y + shift_y))
-
 
 class Game_sprite(pygame.sprite.Sprite):
     def __init__(self, x, y, width, height, image):
@@ -168,6 +171,8 @@ class Bullet(Game_sprite):
 
 btn1 = Button(100, 100, 200, 100, "Level 1")
 btn2 = Button(400, 100, 200, 100, "Level 2")
+menu = Button(250, 400, 170, 70, "Menu")
+
 
 enemys = pygame.sprite.Group()
 bullets = pygame.sprite.Group()
@@ -178,10 +183,13 @@ arrow = Game_sprite(350, 300, 80, 100, 'images//arrow.png')
 
 music_shot = pygame.mixer.Sound('music//shot.ogg')
 
+stat_menu = 0
 level = 0
 start_level_0 = 0
 start_level_1 = 0
 start_level_2 = 0
+time_sleep_st = 0
+time_sleep_end = 0
 
 play = True
 game = True
@@ -216,7 +224,7 @@ while game:
             if event.key == pygame.K_DOWN or event.key == pygame.K_s:
                 player.speed_y = 0
 
-        #меню
+        #стартове меню
         if level == 0:
             if event.type == pygame.MOUSEMOTION:
                 x, y = event.pos
@@ -224,9 +232,12 @@ while game:
                     btn1.color = LIGHT_BLUE
                 elif btn2.rect.collidepoint(x, y):
                     btn2.color = LIGHT_BLUE
+                elif menu.rect.collidepoint(x, y):
+                    menu.color = LIGHT_BLUE
                 else:
                     btn1.color = BLUE
                     btn2.color = BLUE
+                    menu.color = BLUE
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if btn1.rect.collidepoint(x, y):
                     level = 1
@@ -236,16 +247,27 @@ while game:
                     level = 2
                     music_button_use.play()
 
+                if menu.rect.collidepoint(x, y):
+                    if stat_menu == 0:
+                        stat_menu = 1
+                    else:
+                        stat_menu = 0
+                    print('stat_menu:', stat_menu)
+                    music_button_use.play()
 
-
-    if level == 0:
+    # загрузка главного меню (всі елементи)
+    if level == 0: 
         if start_level_0 == 0:
             player = Player(315, 250, 75, 80, 'images//hunter.png', 0, 0)
             start_level_0 = 1
         
+        if stat_menu == 1:
+            pass
+
         window.fill(GREY)
         btn1.show()
         btn2.show()
+        menu.show()
         player.reset()
         player.update()
         bullets.draw(window)
@@ -290,12 +312,16 @@ while game:
                 level = 2
                 pygame.mixer.music.stop()
                 music_pick_up_coin.play()
-                sleep(1.2)
-                music_pick_up_coin.stop()
+                time_sleep_end = time() + 1.5
+                time_sleep_st = time()
+                window.blit(next_level_image, (0, 0))
+                pygame.display.update()
+                while not time_sleep_st >= time_sleep_end:
+                    time_sleep_st = time()
+                    if time_sleep_st >= time_sleep_end:
+                        music_pick_up_coin.stop()
 
-
-
-
+                
             if pygame.sprite.spritecollide(player, enemys, False):
                 play = False
                 window.blit(gg_image, (0, 0))
@@ -306,61 +332,66 @@ while game:
             pygame.sprite.groupcollide(bullets, enemys, True, True)
 
     elif level == 2:
+        
         # загрузка 2 ур
         if start_level_2 == 0:
             pygame.mixer.music.load('music//fon_music(2).ogg')
             pygame.mixer.music.play()
-            player = Player(100, 50, 75, 80, 'images//hunter.png', 0, 0)
+            player = Player(620, 400, 75, 80, 'images//hunter.png', 0, 0)
             play = True
             enemys.empty()
-            enemys.add(Enemy(400, 300, 90, 90, 'images//warior.png', 8, 'left', 420, 700))
-            enemys.add(Enemy(600, 98, 90, 90, 'images//warior.png', 5, 'right', 330, 700))
+            enemys.add(Enemy(10, 5, 90, 90, 'images//warior_L2.png', 8, 'right', 0, 400))
+            enemys.add(Enemy(0, 405, 90, 90, 'images//warior_L2.png', 10, 'right', 0, 560))
             walls.empty()
-            walls.add(Game_sprite(0, 190, 120, 30, 'images//wall.png'))
-            walls.add(Game_sprite(120, 190, 120, 30, 'images//wall.png'))
-            walls.add(Game_sprite(240, 190, 170, 30, 'images//wall.png'))
-            walls.add(Game_sprite(310, 110, 30, 250, 'images//wall.png'))
-            walls.add(Game_sprite(240, 360, 180, 30, 'images//wall.png'))
-            walls.add(Game_sprite(580, 190, 120, 30, 'images//wall.png'))
-            walls.add(Game_sprite(120, 360, 120, 30, 'images//wall.png'))
-            coin = Game_sprite(240,  280, 50, 50, 'images//coin.png')
+            walls.add(Game_sprite(560, 100, 30, 400, 'images//wall.png'))
+            walls.add(Game_sprite(400, 0, 30, 260, 'images//wall.png'))
+            walls.add(Game_sprite(280, 230, 150, 30, 'images//wall.png'))
+            walls.add(Game_sprite(280, 230, 30, 150, 'images//wall.png'))
+            walls.add(Game_sprite(140, 370, 290, 30, 'images//wall.png'))
+            walls.add(Game_sprite(140, 100, 30, 150, 'images//wall.png'))
+            walls.add(Game_sprite(0, 220, 140, 30, 'images//wall.png'))
+            walls.add(Game_sprite(140, 100, 170, 30, 'images//wall.png'))
+
+            coin = Game_sprite(50,  140, 50, 50, 'images//coin.png')
             start_level_2 = 1
         
 
-        if play:
-            window.blit(img_level_2, (0, 0))
-            player.reset()
-            player.update()
-            enemys.draw(window)
-            enemys.update()
-            walls.draw(window)
-            coin.reset()
-            bullets.draw(window)
-            bullets.update()
-            
-            #arrow.reset()
+        if time_sleep_st >= time_sleep_end:
+            music_pick_up_coin.stop()
+            if play:
+                window.blit(img_level_2, (0, 0))
+                player.reset()
+                player.update()
+                enemys.draw(window)
+                enemys.update()
+                walls.draw(window)
+                coin.reset()
+                bullets.draw(window)
+                bullets.update()
+                
+                #arrow.reset()
 
-            if pygame.sprite.collide_rect(player, coin):
-                play = False
-                level = 2
-                pygame.mixer.music.stop()
-                music_pick_up_coin.play()
-                sleep(1.2)
-                music_pick_up_coin.stop()
-                pygame.mixer.music.play()
+                if pygame.sprite.collide_rect(player, coin):
+                    window.blit(win_image, (0, 0))
+                    play = False
+                    level = 3
+                    pygame.mixer.music.stop()
+                    music_win.play()
+                    
 
 
 
 
-            if pygame.sprite.spritecollide(player, enemys, False):
-                play = False
-                window.blit(gg_image, (0, 0))
-                pygame.mixer.music.stop()
-                music_gg.play()
+                if pygame.sprite.spritecollide(player, enemys, False):
+                    play = False
+                    window.blit(gg_image, (0, 0))
+                    pygame.mixer.music.stop()
+                    music_gg.play()
 
-            pygame.sprite.groupcollide(bullets, walls, True, False)
-            pygame.sprite.groupcollide(bullets, enemys, True, True)
-
+                pygame.sprite.groupcollide(bullets, walls, True, False)
+                pygame.sprite.groupcollide(bullets, enemys, True, True)
+        else:
+            time_sleep_st = int(time())
 
 
         
